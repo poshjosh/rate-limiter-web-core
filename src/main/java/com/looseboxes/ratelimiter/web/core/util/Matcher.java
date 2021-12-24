ebbbe9e54a5cfc8e83e563c3bb28f45470be6e31
chinomso <posh.bc@gmail.com>
2021-12-24 20:13:00 +0100

    Remove YAGNI method

--- src/main/java/com/looseboxes/ratelimiter/web/core/util/Matcher.java
+++ src/main/java/com/looseboxes/ratelimiter/web/core/util/Matcher.java
@@ -1,7 +1,5 @@
 package com.looseboxes.ratelimiter.web.core.util;
 
-import com.looseboxes.ratelimiter.util.Nullable;
-
 public interface Matcher<T, K> {
 
     Matcher<Object, Object> MATCH_NONE = (target, resultIfNone) -> resultIfNone;
@@ -9,10 +7,6 @@ public interface Matcher<T, K> {
     @SuppressWarnings("unchecked")
     static <T, K> Matcher<T, K> matchNone() {
         return (Matcher<T, K>)MATCH_NONE;
-    }
-
-    default @Nullable K match(T target) {
-        return getKeyIfMatchingOrDefault(target, null);
     }
 
     K getKeyIfMatchingOrDefault(T target, K resultIfNone);
